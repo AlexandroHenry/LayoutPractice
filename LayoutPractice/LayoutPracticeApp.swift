9d4654ab0e72a5263f4b8005d4669e013cbc256f
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-16 16:42:11 +0900

    Initial Commit

---
+++ LayoutPractice/LayoutPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutPracticeApp.swift
+//  LayoutPractice
+//
+//  Created by Seungchul Ha on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct LayoutPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
